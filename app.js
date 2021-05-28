65a824c7f69d687f82547134f0a3fdb7b5ddd5b3
Will Woodward <will@woodwardweb.com>
2021-05-28 22:21:53 +0100

    Updates

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
-function darkMode(mode) {
-    if (mode == 'dark') {
-        document.querySelector(':root').style.cssText = '--darkBackground: rgb(23, 23, 23); --darkText: #ffffff; --progressBackground: rgb(53, 53, 53);';
-    } else if (mode == 'light') {
-        document.querySelector(':root').style.cssText = '--darkBackground: rgb(255, 255, 255); --darkText: #000000; --progressBackground: rgb(222, 222, 222);';
+let darkMode = true;
+
+let toggleDarkMode = function () {
+    if (darkMode === false) {
+        document.querySelector(':root').style.cssText = '--darkBackground: rgb(23, 23, 23); --darkText: #ffffff; --darkCardBackground: rgb(33, 33, 33); --progressBackground: rgb(53, 53, 53);';
+        document.querySelector('.toggle i.switch').style.cssText = 'visibility: visible;';
+        document.querySelector('.toggle i.swap').style.cssText = 'visibility: hidden;';
+        darkMode = true;
+    } 
+    
+    else if (darkMode == true) {
+        document.querySelector(':root').style.cssText = '--darkBackground: rgb(255, 255, 255); --darkText: #000000; --darkCardBackground: rgb(243,243, 243); --progressBackground: rgb(222, 222, 222);';
+        document.querySelector('.toggle i.switch').style.cssText = 'visibility: hidden;';
+        document.querySelector('.toggle i.swap').style.cssText = 'visibility: visible;';
+        darkMode = false;
     }
-}
+};
+
+let checkDarkMode = function () {
+    if (darkMode === true) {
+        document.querySelector(':root').style.cssText = '--darkBackground: rgb(23, 23, 23); --darkText: #ffffff; --darkCardBackground: rgb(33, 33, 33); --progressBackground: rgb(53, 53, 53);';
+        document.querySelector('.toggle i.switch').style.cssText = 'visibility: visible;';
+        document.querySelector('.toggle i.swap').style.cssText = 'visibility: hidden;';
+    } 
+    
+    else if (darkMode == false) {
+        document.querySelector(':root').style.cssText = '--darkBackground: rgb(255, 255, 255); --darkText: #000000; --darkCardBackground: rgb(243,243, 243); --progressBackground: rgb(222, 222, 222);';
+        document.querySelector('.toggle i.switch').style.cssText = 'visibility: hidden;';
+        document.querySelector('.toggle i.swap').style.cssText = 'visibility: visible;';
+    }
+};
+
+window.onload = function() {
+    checkDarkMode();
+};
